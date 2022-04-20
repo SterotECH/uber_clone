1fc2092999ff412b66e06b32603da72ebf8d4d95
stero_tECH <88740526+SterotECH@users.noreply.github.com>
2022-04-20 05:26:43 +0000

    Bottom Navigation Button
    This implements the TabView Widget in Flutter it has 4 tabs

--- lib/mainScreens/main_screen.dart
+++ lib/mainScreens/main_screen.dart
@@ -1,18 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:uber_clone/tabPages/earning_tab.dart';
+import 'package:uber_clone/tabPages/home_tab.dart';
+import 'package:uber_clone/tabPages/profile_tab.dart';
+import 'package:uber_clone/tabPages/ratings_tab.dart';
 
 class MainScreen extends StatefulWidget {
-  MainScreen({Key? key}) : super(key: key);
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
+  TabController? tabController;
+  int selectedIndex = 0;
+
+  onItemClicked(int index) {
+    setState(() {
+      selectedIndex = index;
+      tabController!.index = selectedIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    tabController = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome"),
+      body: TabBarView(
+        controller: tabController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          HomeTabPage(),
+          EarningTabPage(),
+          RatingsTabPage(),
+          ProfileTabPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white54,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.blueGrey[900],
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(fontSize: 14),
+        showUnselectedLabels: true,
+        currentIndex: selectedIndex,
+        onTap: onItemClicked,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "Earnings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Ratings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          )
+        ],
       ),
     );
   }
---
+++ lib/tabPages/earning_tab.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EarningTabPage extends StatefulWidget {
+  const EarningTabPage({Key? key}) : super(key: key);
+
+  @override
+  State<EarningTabPage> createState() => _EarningTabPageState();
+}
+
+class _EarningTabPageState extends State<EarningTabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Earnings Tab Page",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
